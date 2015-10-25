1a24adea7b361bc11b74b4d7d1666c6a53595fa5
zu.ming.tan <zooming.tan@gmail.com>
2015-10-25 18:38:06 +0800

    Install matchit.vim

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@
 # vim
 ln -s $(pwd)/vimrc $HOME/.vimrc
 
+# jump to opening/closing tags by typing %
+mkdir -p $HOME/.vim/plugin
+VIMRUNTIME=`vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015' `
+cp $VIMRUNTIME/macros/matchit.vim $HOME/.vim/plugin
+
 # tmux
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 
